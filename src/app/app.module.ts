720d72db75a9f34da4beb0db2dec14e6ba0c6b04
Ahmad Fahadh Ilyas <fahadhilyas4@gmail.com>
2020-11-23 16:13:51 +0700

    only load some module from ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCollapseModule, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { AboutComponent } from './components/about/about.component';
 import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 import { TopNavbarComponent } from './components/top-navbar/top-navbar.component';
@@ -22,7 +22,8 @@ import { FooterComponent } from './components/footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbCollapseModule,
+    NgbDropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
