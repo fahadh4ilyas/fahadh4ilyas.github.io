11731744b816c1f353309c185e06ade84ebd91ea
Ahmad Fahadh Ilyas <fahadhilyas4@gmail.com>
2020-11-26 11:46:34 +0700

    add pagenotfound title

--- src/app/components/pagenotfound/pagenotfound.component.ts
+++ src/app/components/pagenotfound/pagenotfound.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-pagenotfound',
@@ -13,9 +14,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagenotfoundComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _title: Title) { }
 
   ngOnInit(): void {
+    this._title.setTitle('404: Page Not Found')
   }
 
 }
